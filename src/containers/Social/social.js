40e4d78fb342b373d6e8161ef4ccad4cd3b40161
Kenneth Chewy <ken88there@gmail.com>
2017-12-12 18:36:05 -0800

    Revert "adding node modules given some custom fixes for them"
    This reverts commit abdc704c832f77cca50692eb908c52d930f9d8da.

--- src/containers/Social/social.js
+++ src/containers/Social/social.js
@@ -4,7 +4,7 @@ import 'react-twitter-widgets';
 import { Carousel } from 'react-responsive-carousel';
 
 const style = require('./social.scss');
-const instagramName = 'kchewy89';
+const instagramName = 'amandaonthemoon';
 
 class Social extends Component {
 
@@ -44,16 +44,20 @@ class Social extends Component {
   }
 
   instaSideCar = (json) => {
+    console.log('instasidecar');
     const nodeImages = json.user.media.nodes;
     for (let node = 0; node < nodeImages.length; node++) {
       if (nodeImages[node].__typename === 'GraphSidecar') {
+        console.log('sidecar');
         this.callSideCar(json, node);
       } else if (nodeImages[node].__typename === 'GraphVideo') {
+        console.log('GraphVideo');
         this.callGraphVideo(json, node);
       } else if (nodeImages[node].__typename === 'GraphImage') {
         console.log('regular image');
       }
       if (node === nodeImages.length - 1) {
+        console.log('finishing up');
         this.setState({
           biography: json.user.biography,
           followed_by: json.user.followed_by,
@@ -91,7 +95,9 @@ class Social extends Component {
       return response.text();
     })
     .then( (html) => {
+      console.log('sidecar111');
       const json11 = JSON.parse(html.split('window._sharedData = ')[1].split(';</script>')[0]);
+      console.log(json11);
       const sidecarImages = [];
       const edgeNodes = json11.entry_data.PostPage[0].graphql.shortcode_media.edge_sidecar_to_children.edges;
       for (let nnn = 0; nnn < edgeNodes.length; nnn++) {
@@ -116,9 +122,11 @@ class Social extends Component {
   }
 
   instaList = () => {
+    console.log('isntalist');
     const url = 'https://www.instagram.com/' + instagramName + '/?__a=1';
     fetch(url)
     .then(function(response) {
+      console.log(response);
       return response.json();
     }).then( (json) => {
       for (let nnn = 0; nnn < json.user.media.nodes.length; nnn++) {
@@ -131,11 +139,13 @@ class Social extends Component {
   }
 
   render() {
+    console.log('render');
     const medias = ___.map(this.state.media, (media) => {
       const imagesPossible = [];
       if (media.__typename === 'GraphSidecar') {
         const sidecarImages = [];
         for (let ima = 0; ima < media.sidecar01.length; ima++) {
+          console.log(media.sidecar01[ima]);
           if (media.sidecar01[ima].video_url) {
             sidecarImages.push(
               <div>
@@ -162,6 +172,7 @@ class Social extends Component {
                 {sidecarImages}
               </Carousel>);
       } else if (media.__typename === 'GraphVideo') {
+        console.log(media.graphVideo);
         imagesPossible.push(
           <video width="530" height="530" src={media.graphVideo} controls>
           </video>
@@ -170,10 +181,12 @@ class Social extends Component {
         imagesPossible.push(<img src={media.display_src} alt="nothing yet" />);
       }
       const captionLines = [];
+      console.log(media.caption);
       if (typeof media.caption !== 'undefined') {
         if ((media.caption).indexOf('#') > -1 || (media.caption).indexOf('@') > -1) {
           const c1 = ' ' + media.caption;
           const caption = c1.split('#');
+          console.log(caption);
           let phrase = '';
           for (phrase in caption) {
             if (phrase > 0 || (phrase === 0 && media.caption[1] === '#')) {
@@ -202,6 +215,7 @@ class Social extends Component {
               }
             } else {
               const userList = caption[phrase].split(' @');
+              console.log(userList);
               captionLines.push(<span>{userList[0]}</span>);
               let user1 = '';
               for (user1 in userList) {
