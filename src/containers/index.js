6ca270ce3f042cff309fc7e9b207b06aa6f59605
Kenneth Chewy <ken88there@gmail.com>
2017-12-13 18:07:07 -0800

    missed bug

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,6 @@
 export App from './App/App';
 export Bio from './Bio/bio';
+export Contact from './Contact/contact';
 export Home from './Home/Home';
 export Music from './Music/music';
 export Social from './Social/social';
