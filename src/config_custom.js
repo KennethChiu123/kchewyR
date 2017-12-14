e804b88eb3ce0dad352692d71c662ee4419258e6
Kenneth Chewy <ken88there@gmail.com>
2017-12-14 15:33:23 -0800

    update configs for kchewy

--- src/config_custom.js
+++ src/config_custom.js
@@ -2,27 +2,27 @@ require('babel-polyfill');
 
 module.exports = Object.assign({
   app: {
-    title: 'amandaonthemoon',
+    title: 'kchewy',
     type: 'musicRef',
     home: {
-      facebook_href: 'https://www.facebook.com/amanda.carson.9847/',
-      twitter_href: 'https://twitter.com/#!/a_onthemoon',
-      youtube_href: 'http://www.youtube.com/user/amandacarsonmusic',
-      instagram_href: 'http://instagram.com/amandaonthemoon',
+      facebook_href: 'https://www.facebook.com/kenneth.chiu.3975/',
+      twitter_href: 'https://twitter.com/#!/kchewy89',
+      youtube_href: 'http://www.youtube.com/user/ken88there',
+      instagram_href: 'http://instagram.com/kchewy89',
       spotify_href: '',
       soundcloud_href: 'http://www.soundcloud.com/amandaonthemoon',
       vimeo_href: '',
     },
     bio: {
-      bioTitle1: 'Amanda Carson',
-      bioTitle2: 'Singer/songwriter, born and raised in Los Angeles',
-      bioDescription: 'Combining vivid and intimately human lyrics with a sultry folk sound, Amanda draws much of her inspiration from artists like The Civil Wars, Laura Marling, Brandi Carlile, and Gregory Alan Isakov. After playing with numerous bands and comprising one half of the folk duo, Buttonwillow Locomotive, Amanda Carson\'s latest endeavor has been a solo project, and she can often be found playing at Venues around Hollywood and West Hollywood.',
+      bioTitle1: 'KChewy',
+      bioTitle2: 'Developer, knows Java, coffee optional.',
+      bioDescription: 'Born and Raised in LA. I did some stand up comedy up in Berkeley and I\'ve flirted with it ever since.'
     },
     music: {
       youtube_channel_id: 'UCA95A2hb8QPRTeV9C74uToQ',
     },
     social: {
-      instagram_account: 'amandaonthemoon',
+      instagram_account: 'kchewy89',
     },
     contact: {
       email_address: 'ken88there@gmail.com',
